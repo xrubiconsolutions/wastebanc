fedfc7aef2cf44d84d4d7f9478a8aa6259d36bdf
Bukola <sobowalebukola@gmail.com>
2021-06-16 10:13:38 +0100

    ...

--- server/controller/scheduleDropController.js
+++ server/controller/scheduleDropController.js
@@ -5,6 +5,37 @@ let MODEL = require("../models");
 let COMMON_FUN = require("../util/commonFunction");
 let CONSTANTS = require("../util/constants");
 
+var request = require('request');
+
+const OneSignal = require('onesignal-node');
+
+var sendNotification = function (data) {
+  var headers = {
+    'Content-Type': 'application/json; charset=utf-8',
+  };
+
+  var options = {
+    host: 'onesignal.com',
+    port: 443,
+    path: '/api/v1/notifications',
+    method: 'POST',
+    headers: headers,
+  };
+
+  var https = require('https');
+  var req = https.request(options, function (res) {
+    res.on('data', function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  req.on('error', function (e) {
+    console.log(e);
+  });
+
+  req.write(JSON.stringify(data));
+  req.end();
+};
 scheduleDropController.schedule = (REQUEST, RESPONSE) => {
   var data = { ...REQUEST.body };
 
