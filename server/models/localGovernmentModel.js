af249a519cadc95e295c73dd926c59fd0990c497
Bukola <sobowalebukola@gmail.com>
2021-07-15 13:48:52 +0100

    lcd new model, get lcd and upload lcda by admin

---
+++ server/models/localGovernmentModel.js
@@ -0,0 +1,22 @@
+"use strict";
+/************* Modules ***********/
+const MONGOOSE = require("mongoose");
+const Schema = MONGOOSE.Schema;
+const Constants = require("../util/constants");
+
+/**************************************************
+ ************* localGovernment Model or collection ***********
+ **************************************************/
+
+
+const localGovernment_Schema = new Schema({
+  lcd: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = MONGOOSE.model("localGovernment", localGovernment_Schema);
