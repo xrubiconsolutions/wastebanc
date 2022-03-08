ddf76bd458a07b6eebe7116cae69ccc3be18d06d
james <uchennaanih16@gmail.com>
2022-03-09 00:49:46 +0100

    merge collectors

--- server/routes/v2/collectorRoutes.js
+++ server/routes/v2/collectorRoutes.js
@@ -2,17 +2,18 @@ const CollectorService = require("../../controllerv2/collectorController.js");
 const adminAuthorization = require("../../middleware/adminAuthorization.js");
 const { checkRequestErrs } = require("../../util/commonFunction.js");
 const commonValidator = require("../../validators/commonValidator.js");
+const auth = require("../../util/auth");
 
 module.exports = (APP) => {
   APP.route("/api/v2/collectors").get(
-    adminAuthorization(),
+    auth.adminPakamValidation,
     commonValidator.filter,
     checkRequestErrs,
     CollectorService.getCollectors
   );
 
   APP.route("/api/v2/collectors/search").get(
-    adminAuthorization(),
+    auth.adminPakamValidation,
     commonValidator.search,
     checkRequestErrs,
     CollectorService.searchCollectors
