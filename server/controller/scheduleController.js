1f4b17a27d9fd806bb5d07617d295eba2e04cdc1
james <uchennaanih16@gmail.com>
2022-03-17 17:51:01 +0100

    added console

--- server/controller/scheduleController.js
+++ server/controller/scheduleController.js
@@ -99,6 +99,7 @@ scheduleController.schedule = (REQUEST, RESPONSE) => {
     const expireDate = moment(data.pickUpDate, "YYYY-MM-DD").add(7, "days");
     data.expiryDuration = expireDate;
     data.clientId = result._id;
+    console.log('sch', data);
     MODEL.scheduleModel(data).save({}, (ERR, RESULT) => {
       try {
         if (ERR) return RESPONSE.status(400).jsonp(COMMON_FUN.sendError(ERR));
