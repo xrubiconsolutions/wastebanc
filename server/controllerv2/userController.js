9a98858e0c5e1b1fa36273c53b14b87025612898
james <uchennaanih16@gmail.com>
2022-04-14 17:18:43 +0100

    fix

--- server/controllerv2/userController.js
+++ server/controllerv2/userController.js
@@ -593,20 +593,22 @@ class UserService {
           },
         });
       }
+      let signal_id;
 
       console.log("user id", user.onesignal_id);
       if (user.onesignal_id === "" || user.onesignal_id === " ") {
-        const id = uuid.v1().toString();
+        signal_id = uuid.v1().toString();
         console.log("changing", id);
         await userModel.updateOne(
           { email: user.email },
-          { $set: { last_logged_in: new Date(), onesignal_id: id } }
+          { $set: { last_logged_in: new Date(), onesignal_id: signal_id } }
         );
       } else {
+        signal_id = user.onesignal_id;
         console.log("not changing", user.onesignal_id);
         await userModel.updateOne(
           { email: user.email },
-          { $set: { last_logged_in: new Date(), onesignal_id: id } }
+          { $set: { last_logged_in: new Date() } }
         );
       }
       // console.log("signalid", signal_id);
