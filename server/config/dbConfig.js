a8bb2191697c936adbb9a18614aa0442b59fd832
james <uchennaanih16@gmail.com>
2022-10-23 13:55:36 +0100

    fix

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -8,40 +8,38 @@
  ****** Default Server configuration ******
  ******************************************/
 let serverConfig = {
-    mongodb: {
-        host        : "127.0.0.1",
-        port        : 27017,
-        name        : "mongodb",
-        connector   : "mongodb", 
-        url         : process.env.dbUrl || "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
-        database    : "demo",
-        user        : "",
-        password    : "",
-    },
-    host    : "localhost",
-    type    : "http://",
-    port    : process.env.PORT || '4002'
+  mongodb: {
+    host: "127.0.0.1",
+    port: 27017,
+    name: "mongodb",
+    connector: "mongodb",
+    url:
+      process.env.dbUrl ||
+      "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
+    database: "demo",
+    user: "",
+    password: "",
+  },
+  host: "localhost",
+  type: "http://",
+  port: process.env.PORT || "4002",
 };
-
 
 /***********************************
  ** Maintain server Configuration **
  **** according to env variable ****
  ***********************************/
-if(process.env.NODE_ENV === "development"){   
-    serverConfig.mongodb.user           =   "";
-    serverConfig.mongodb.password       =   "";
-}
-else if( process.env.NODE_ENV === "production"){
-    serverConfig.mongodb.url            =   "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority" ;
-    serverConfig.mongodb.database       =   "demo";
-    serverConfig.mongodb.user           =   "";
-    serverConfig.mongodb.password       =   "";
-    serverConfig.port                   =   process.env.PORT || "4002";
+if (process.env.NODE_ENV === "development") {
+  serverConfig.mongodb.user = "";
+  serverConfig.mongodb.password = "";
+} else if (process.env.NODE_ENV === "production") {
+  serverConfig.mongodb.url =
+    "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority";
+  serverConfig.mongodb.database = "demo";
+  serverConfig.mongodb.user = "";
+  serverConfig.mongodb.password = "";
+  serverConfig.port = process.env.PORT || "4002";
 }
 
 /** exporting server configuration **/
 module.exports = serverConfig;
-
-
-
