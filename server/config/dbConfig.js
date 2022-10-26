f3b8755e994f4e6b1124c06e791cd89c1b74f118
james <uchennaanih16@gmail.com>
2022-10-26 11:31:48 +0100

    f

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -15,30 +15,31 @@ let serverConfig = {
     connector: "mongodb",
     url:
       process.env.dbUrl ||
-      "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
-    database: "demo",
+      "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
+    database: "pakam",
     user: "",
     password: "",
   },
   host: "localhost",
   type: "http://",
-  port: process.env.PORT || "4002",
+  port: process.env.PORT || "4000",
 };
 
 /***********************************
  ** Maintain server Configuration **
  **** according to env variable ****
  ***********************************/
+
 if (process.env.NODE_ENV === "development") {
   serverConfig.mongodb.user = "";
   serverConfig.mongodb.password = "";
 } else if (process.env.NODE_ENV === "production") {
   serverConfig.mongodb.url =
-    "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority";
-  serverConfig.mongodb.database = "demo";
+    "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority";
+  serverConfig.mongodb.database = "pakam";
   serverConfig.mongodb.user = "";
   serverConfig.mongodb.password = "";
-  serverConfig.port = process.env.PORT || "4002";
+  serverConfig.port = process.env.PORT || "4001";
 }
 
 /** exporting server configuration **/
