4f15324ecd95d91813729cc668ecf00064df71b0
jamesthcohq <jamesthcohq.com>
2023-01-30 14:17:14 +0100

    fix

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -15,7 +15,7 @@ let serverConfig = {
     connector: "mongodb",
     url:
       process.env.dbUrl ||
-      "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam-live?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
+      "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority",
     database: "pakam",
     user: "",
     password: "",
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
   serverConfig.mongodb.password = "";
 } else if (process.env.NODE_ENV === "production") {
   serverConfig.mongodb.url =
-    "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/pakam-live?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority";
+    "mongodb://pakamXrubicon:me202010*@pakam-shard-00-00.wcabw.mongodb.net:27017,pakam-shard-00-01.wcabw.mongodb.net:27017,pakam-shard-00-02.wcabw.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-13j6g2-shard-0&authSource=admin&retryWrites=true&w=majority";
   serverConfig.mongodb.database = "pakam";
   serverConfig.mongodb.user = "";
   serverConfig.mongodb.password = "";
