b74d5f30f0daa1d789454885267a060a8024a345
jamesthcohq <jamesthcohq.com>
2023-04-17 14:39:49 +0100

    updated inusranceExpiryDate

--- server/modules/partners/mycover.io/mycoverService.js
+++ server/modules/partners/mycover.io/mycoverService.js
@@ -133,7 +133,7 @@ const buyHealthInsurance = async (data, userId) => {
         insurancePolicyID: result.data.data.policy.id,
         availablePoints: userBalance,
         insuranceAmount: amount,
-        // insuranceExpiryDate: moment().add(plan_duration, "months").calendar(),
+        insuranceExpiryDate: result.data.data.policy.expiration_date,
       }
     );
 
