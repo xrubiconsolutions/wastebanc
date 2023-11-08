7eebc689d92cf54e157671c12b590cf2e8a5e636
james234 <uchennaanih16@gmail.com>
2023-11-08 20:01:20 +0100

    updates

--- server/routes/v1/payRoute.js
+++ server/routes/v1/payRoute.js
@@ -9,44 +9,45 @@ const payValidator = require("../../validators/payValidators.js");
  ***** @param APP (express instance)*****
  ****************************************/
 module.exports = (APP) => {
-  APP.route("/api/all/banks").get(CONTROLLER.payController.getBanks);
+	APP.route("/api/all/banks").get(CONTROLLER.payController.getBanks);
 
-  // account number resolver
-  APP.route("/api/resolve/account").get(
-    auth.userValidation,
-    CONTROLLER.payController.resolveAccount
-  );
+	// account number resolver
+	APP.route("/api/resolve/account").get(
+		auth.userValidation,
+		CONTROLLER.payController.resolveAccount
+	);
 
-  APP.route("/api/admin/resolve/account").get(
-    auth.adminPakamValidation,
-    CONTROLLER.payController.resolveAccount
-  );
+	APP.route("/api/admin/resolve/account").get(
+		auth.adminPakamValidation,
+		CONTROLLER.payController.resolveAccount
+	);
 
-  APP.route("/api/payment/receipt").post(
-    auth.userValidation,
-    CONTROLLER.payController.saveR
-  );
+	APP.route("/api/payment/receipt").post(
+		auth.userValidation,
+		CONTROLLER.payController.saveR
+	);
 
-  APP.route("/api/payment/user/current").get(
-    CONTROLLER.payController.afterPayment
-  );
+	APP.route("/api/payment/user/current").get(
+		auth.userValidation,
+		CONTROLLER.payController.afterPayment
+	);
 
-  APP.route("/api/requested/payout").get(
-    auth.companyValidation,
-    CONTROLLER.payController.requestedPayment
-  );
-  APP.route("/api/payout/history").get(
-    auth.adminPakamValidation,
-    CONTROLLER.payController.allPayoutHistory
-  );
-  APP.route("/api/update/payment").post(
-    auth.companyValidation,
-    CONTROLLER.payController.paymentUpdate
-  );
-  APP.route("/api/charity/payment").post(
-    auth.userValidation,
-    payValidator.charityPay,
-    checkRequestErrs,
-    CONTROLLER.payController.charityP
-  );
+	APP.route("/api/requested/payout").get(
+		auth.companyValidation,
+		CONTROLLER.payController.requestedPayment
+	);
+	APP.route("/api/payout/history").get(
+		auth.adminPakamValidation,
+		CONTROLLER.payController.allPayoutHistory
+	);
+	APP.route("/api/update/payment").post(
+		auth.companyValidation,
+		CONTROLLER.payController.paymentUpdate
+	);
+	APP.route("/api/charity/payment").post(
+		auth.userValidation,
+		payValidator.charityPay,
+		checkRequestErrs,
+		CONTROLLER.payController.charityP
+	);
 };
