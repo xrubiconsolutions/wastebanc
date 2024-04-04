3f2981af1fd49c9f49bea6712d057ce4fed9fc22
james234 <uchennaanih16@gmail.com>
2024-04-04 08:25:31 +0100

    upgrade

--- server/models/geofenceModel.js
+++ server/models/geofenceModel.js
@@ -1,7 +1,7 @@
 "use strict";
 /************* Modules ***********/
 const MONGOOSE = require("mongoose");
-const TrustedComms = require("twilio/lib/rest/preview/TrustedComms");
+//const TrustedComms = require("twilio/lib/rest/preview/TrustedComms");
 const Schema = MONGOOSE.Schema;
 const Constants = require("../util/constants");
 
@@ -12,20 +12,20 @@ const Constants = require("../util/constants");
 // expiry.setDate(expiry.getDate() + 365 );
 
 const geofence_Schema = new Schema(
-  {
-    organisationId: {
-      type: String,
-      required: true,
-    },
-    data: {
-      type: Object,
-      required: true,
-    },
-    // createdAt: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-  },
-  { timestamps: true }
+	{
+		organisationId: {
+			type: String,
+			required: true,
+		},
+		data: {
+			type: Object,
+			required: true,
+		},
+		// createdAt: {
+		//   type: Date,
+		//   default: Date.now,
+		// },
+	},
+	{ timestamps: true }
 );
 module.exports = MONGOOSE.model("geofence", geofence_Schema);
